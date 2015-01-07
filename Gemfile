093c508ceba7a572218ae05bb3aee9c1942f8525
Vinicius <vinibol12@yahoo.com.br>
2015-01-08 10:46:41 +1300

    Users scaffold created. Missing bcrypt fixing.

--- Gemfile
+++ Gemfile
@@ -25,8 +25,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+#Use ActiveModel has_secure_password
+gem 'bcrypt'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
@@ -36,6 +36,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+
+
   gem 'byebug'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105214122) do
+ActiveRecord::Schema.define(version: 20150107042144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,13 @@ ActiveRecord::Schema.define(version: 20150105214122) do
   add_index "queue_groceries", ["basket_id"], name: "index_queue_groceries_on_basket_id", using: :btree
   add_index "queue_groceries", ["order_id"], name: "index_queue_groceries_on_order_id", using: :btree
   add_index "queue_groceries", ["product_id"], name: "index_queue_groceries_on_product_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   add_foreign_key "queue_groceries", "orders"
 end
