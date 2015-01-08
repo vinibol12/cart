6e913d6322d5316b1e758f6a27eeb5fe9a1aa0a6
Vinicius <vinibol12@yahoo.com.br>
2015-01-09 00:43:23 +1300

    mailer change in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "domain.of.sender.net",
+      authentication: "plain",
+      user_name: "vinisem",
+      password: "Mae041254",
+      enable_starttls_auto: true
+
+  }
 end
+
