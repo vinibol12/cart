133e282f3894dd4505515f71927f56e5d0c3d697
Vinicius <vinibol12@yahoo.com.br>
2015-01-08 17:10:37 +1300

    found problem with Gemfile.old

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
-# Set up gems listed in the Gemfile.old.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile.old', __FILE__)
+# Set up gems listed in the Gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
