03286d9742fb5dd32dbd79af49fcdb7b74eb0f34
Vinicius <vinibol12@yahoo.com.br>
2015-01-08 21:07:00 +1300

    deleted db/migrate/20141227213839_change_index_for_queue_ggroceries.rb

--- db/migrate/20141227213839_change_index_for_queue_ggroceries.rb
+++
@@ -1,6 +0,0 @@
-class ChangeIndexForQueueGgroceries < ActiveRecord::Migration
-  def change
-    rename_index :queue_groceries, :index_queue_groceries_on_cart_id, :index_queue_groceries_on_basket_id
-
-  end
-end
