00e7474c74f871f6ef957b4eb5363c7ea6249ef7
Vinicius <vinibol12@yahoo.com.br>
2015-01-10 14:35:42 +1300

    Authorizarion and access limitation done. Tests erasing db fixed. Clear the problem. No deletion of last user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_action :authorize
+   before_action :authorize
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,22 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  after_destroy :ensure_an_admin_remains
+  # after_destroy is a hook method that calls another another one after an action.
+  # the way it works is if the method passed to after_destroy raises an exception
+  # the last transaction will be rolled back.
+
+  private
+
+  def ensure_an_admin_remains
+
+    if User.count.zero?
+      raise 'Can\'t delete last user. Please go back to the users page.'
+      #once ensure_an_admin_remains makes the count and states that
+      # there is no more users the excepton is raised and the transaction rolled back.
+
+    end
+  end
 
 
 end
